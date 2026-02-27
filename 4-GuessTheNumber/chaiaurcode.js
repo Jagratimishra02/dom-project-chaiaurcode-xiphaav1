5a88494b48d18d403f5f5c356779f04cb9536b03
Jagratimishra02 <nishimisra002@gmail.com>
2026-02-27 16:53:40 +0000

    changes

---
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -0,0 +1,30 @@
+const randomNumber = (parseInt(Math.random()*100+1)) ;
+const submit = document.querySelector('#subt');
+const userinput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const Startover = document.querySelector('.resultParas');
+
+
+const p = document.createElement('p');
+
+let prevGuess = [];
+let numgas = 1;
+let playGame = true;
+
+function validateGuess(guess){
+  //
+}
+
+function checkGuess(guess){
+    //
+}
+
+function displayGuess(){
+    //
+}
+
+function displayMessage(Message){
+ // 
+}
