ee01596ade58caab1f3e48525b87e772c9d9f737
Jagratimishra02 <nishimisra002@gmail.com>
2026-03-02 13:39:41 +0000

    changes

--- 2-BMICalculator/chaiaurcode.js
+++ 2-BMICalculator/chaiaurcode.js
@@ -15,20 +15,20 @@ else if(weight === '' || weight < 0 || isNaN(weight)){
   results.innerHTML = `Please give valid weight ${weight}`;
 
 } else {
- const bmi = (weight / ((height*weight)/10000)).toFixed(2)
+ const bmi = (weight / ((height*height)/10000)).toFixed(2)
 
  results.innerHTML = `<Span>${bmi}</span>`;
+ 
 if (bmi<18.6) 
  {results.innerHTML = `you are underweight ${bmi}`} 
 
-else if(bmi>18.6 || bmi<24.9)
+else if(bmi>=18.6 && bmi<=24.9)
 { results.innerHTML = `you are in normal range ${bmi}` }
 
-  else (bmi>24.9) 
-   {results.innerHTML = `you are over weight ${bmi}`}  
-      }
-
-
+  else 
+   {
+    results.innerHTML = `you are over weight ${bmi}`}  
+}
 
 
  
