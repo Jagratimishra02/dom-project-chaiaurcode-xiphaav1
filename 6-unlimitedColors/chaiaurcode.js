555950cfa27a906cb972862606b6fc3e57b59c3e
Jagratimishra02 <nishimisra002@gmail.com>
2026-03-08 13:17:32 +0000

    updated

--- 6-unlimitedColors/chaiaurcode.js
+++ 6-unlimitedColors/chaiaurcode.js
@@ -4,8 +4,25 @@ const randomcolor = function(){
     const hex = "0123456789ABCDEF";
     let color = '#'
     for(let i = 0; i < 6; i++){
-
+color += hex[Math.floor(Math.random()*16)]
     }
+    return color;
 };
+let intervalId;
+const startChangingColor = function(){ 
+if(intervalId){
+    intervalId =  setInterval(Changebgcolor,1000)}
+    function Changebgcolor(){
+ document.body.style.backgroundColor = randomcolor();
+    }
+    
+};
+const stopChangingColor = function(){
+    clearInterval(intervalId);
+    intervalId = null
+};
+document.querySelector("#start").addEventListener('click',
+    startChangingColor);
+document.querySelector("#stop").addEventListener('click',
+    stopChangingColor)
 
-console.log(Math.random()*16)
